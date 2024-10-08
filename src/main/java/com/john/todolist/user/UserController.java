07037d97b61571f2cae97d95fd44ec37a0f2d426
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-08 17:19:38 -0300

    initial commit

---
+++ src/main/java/com/john/todolist/user/UserController.java
@@ -0,0 +1,32 @@
+package com.john.todolist.user;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.repository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+        }
+
+        var passwordHashed = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
+        var userCreated = this.repository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+}
